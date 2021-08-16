03fecbbb355e6b88e6159dcebd5b0032aa2720d4
Shirina575 <shirina575@gmail.com>
2021-08-16 23:38:31 +0600

    Displaying error message

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const errorMessage = document.getElementById('error-message');
+const meals = document.getElementById('show-meals');
 const findMeal = () => {
     let input = document.getElementById('input').value;
     const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
     if(input === ''){
-        document.getElementById('error-message').innerHTML = `<h3>You didn't search anything!!!`;
+        errorMessage.innerHTML = `<h3>You didn't search anything!!!`;
     }else{
         fetch(api)
         .then(res => res.json())
@@ -11,25 +13,13 @@ const findMeal = () => {
     }
 
 }
-
-// const showMeal = input => {
-//     console.log(input);
-//     input.forEach(meal => {
-//         const div = document.createElement('div');
-//         div.setAttribute('class', 'each-meal')
-//         div.innerHTML = `<img class = "mealThumb" src = "${meal.strMealThumb}">
-//         <h3>${meal.strMeal}</h3>`
-        
-//         document.getElementById('show-meals').appendChild(div)
-//     });
-// }
-
 const showMeal = input => {
     console.log(input);
-    const meals = document.getElementById('show-meals');
     if(input === null){
+        errorMessage.style.display = 'none';
         meals.innerHTML = `<h3>No Meal Found!!!`;
     }else{
+        errorMessage.style.display = 'none';
         meals.innerHTML = ``;
         input.forEach(meal => {
             const div = document.createElement('div');
